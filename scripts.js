2401c161243028e9301705b476a3a92cf5886587
Horacio Sutara <rhsuty@gmail.com>
2025-09-07 19:27:10 -0300

    mover imagen_prueba

--- scripts.js
+++ scripts.js
@@ -1,13 +1,42 @@
 //obtener datos
-const btn_subir= document.getElementById("submit");
+const subir= document.getElementById("submit");
+const img= document.getElementById("imagen");
 const container= document.getElementById("container");
+const end_container= document.getElementById("container_2");
 const msg= document.getElementById("msg");
 
 //funciones
-function saludar(){
-    alert("holas")
+function msg_moviendo(){
+    msg.textContent="Moviendo";
+}
+function mover_elemento(){
+    end_container.appendChild(img); 
+    msg.textContent="";       
+}
+function reset(){
+    container.appendChild(img);
+    msg.textContent="";
+}
+function comenzar(){
+    img.addEventListener("dragstart",msg_moviendo,false);
+
+    
+    end_container.addEventListener("dragover",function(e){
+        e.preventDefault();
+    }, false)
+
+    end_container.addEventListener("drop",mover_elemento,false)
+
+    
+    container.addEventListener("dragover",function(e){
+        e.preventDefault();
+    },false);
+    container.addEventListener("drop",reset,false)
+
+
 }
 
-
 //asignar eventos
-btn_subir.addEventListener("click",saludar);
+window.addEventListener("load",comenzar,false);
+
+
