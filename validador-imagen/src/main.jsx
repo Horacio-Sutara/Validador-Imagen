79149387c4abb8dce51c677078fb6413f947e41b
Horacio Sutara <rhsuty@gmail.com>
2025-09-25 09:01:20 -0300

    creacion de formulario

--- validador-imagen/src/main.jsx
+++ validador-imagen/src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-import "./styles/styles.css";
 import "./styles/cabezera.css";
+import "./styles/styles.css";
+import "./styles/contacto.css"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- validador-imagen/src/pages/contacto.jsx
+++ validador-imagen/src/pages/contacto.jsx
@@ -1,10 +1,35 @@
+import {useRef} from "react";
 
 export default function App() {
+  const form= useRef();
+
+
+  function enviar_email(e){
+    e.preventDefault();
+    form.current.reset();
+
+
+  }
+
   return (
     <>
-        <p>
-            Contacto
-        </p>
+    <div className="contacto">
+      <h1>
+          Contacto
+      </h1>
+      <form className="form" ref={form} onSubmit={enviar_email}>
+        <label className="form_titulo">Nombre: </label>
+        <input className="form_contenido" type="text"  required />
+        
+        <label className="form_titulo">Email: </label>
+        <input className="form_contenido" type="email"  required />
+
+        <label className="form_titulo">Mensaje: </label>
+        <textarea className="form_contenido" name="message" rows="6" required></textarea>
+        <input className="form_boton" type="submit" value="Enviar" />
+
+      </form>
+    </div>
     </>
   );
 }
